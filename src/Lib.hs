6c4272078fd49ebaf8eb317917abb2b609858be8
Jappie Klooster <jappieklooster@hotmail.com>
2018-06-24 17:01:38 +1000

    Setup a simple json api

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,40 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DeriveGeneric #-}
+
 module Lib
     ( webAppEntry
     ) where
 
+import Servant
+import Data.Aeson
+import Data.Aeson.Types
+import GHC.Generics
+import Network.Wai.Handler.Warp
+
+type UserAPI = "users" :> Get '[JSON] [User]
+
+data User = User
+  { name :: String
+  , email :: String
+  } deriving (Eq, Show, Generic)
+
+instance ToJSON User
+
+users :: [User]
+users =
+  [ User "Isaac Newton"    "isaac@newton.co.uk"
+  , User "Albert Einstein" "ae@mc2.org"
+  ]
+
+server :: Server UserAPI
+server = return users
+
+userAPI :: Proxy UserAPI
+userAPI = Proxy
+
+app :: Application
+app = serve userAPI server
+
 webAppEntry :: IO ()
-webAppEntry = putStrLn "This is the beginning of my greetings to world"
+webAppEntry = run 6868 app
