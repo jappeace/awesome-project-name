7de80c15ace214cc049bc6b77b598ac5d2ece239
Jappie Klooster <jappieklooster@hotmail.com>
2018-06-25 11:58:23 +1000

    Changes for handling another endpoint

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,13 +6,25 @@ module Lib
     ( webAppEntry, User
     ) where
 
-import Servant(serve, Proxy(..), Server, JSON, Get, (:>))
-import Data.Aeson(ToJSON)
+import Servant
+import Control.Monad.IO.Class(liftIO)
+import Data.ByteString.Lazy as LBS (writeFile, readFile) 
+import Data.Aeson(ToJSON, FromJSON, encode, decode)
 import GHC.Generics(Generic)
 import Network.Wai(Application)
 import Network.Wai.Handler.Warp(run)
+import Data.Maybe (fromMaybe)
 
 type UserAPI = "users" :> Get '[JSON] [User]
+      :<|> "message" :> ReqBody '[JSON] Message :> Post '[JSON] [Message]
+
+data Message = Message {
+  from :: User,
+  content :: String
+} deriving (Eq, Show, Generic)
+
+instance ToJSON Message
+instance FromJSON Message
 
 data User = User
   { name :: String
@@ -20,15 +32,28 @@ data User = User
   } deriving (Eq, Show, Generic)
 
 instance ToJSON User
+instance FromJSON User
 
 users :: [User]
 users =
   [ User "Isaac Newton"    "isaac@newton.co.uk"
   , User "Albert Einstein" "ae@mc2.org"
   ]
+messageFile :: FilePath
+messageFile = "messages.txt"
+
+messages :: Message -> Handler [Message]
+messages message = do 
+  read <- liftIO $ LBS.readFile messageFile
+  case decode read of
+    Nothing -> pure []
+    Just x -> do
+      let contents = x ++ [message]
+      liftIO $ LBS.writeFile messageFile (encode contents)
+      return contents
 
 server :: Server UserAPI
-server = pure users
+server = (pure users) :<|> messages
 
 userAPI :: Proxy UserAPI
 userAPI = Proxy
