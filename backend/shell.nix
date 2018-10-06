9295a3972d925a18f270187414b2d85b42f9b218
Jappie Klooster <jappieklooster@hotmail.com>
2018-10-06 14:33:50 +0200

    I have no idea what I did last time

--- backend/shell.nix
+++ backend/shell.nix
@@ -1,10 +1,6 @@
 { pkgs ? (import ../reflex-platform/nixpkgs {}) }:
-(pkgs.stdenv.mkDerivation {
+(pkgs.haskell.lib.buildStackProject {
   name = "Minimal-example-of";
-  buildTools = [ pkgs.haskellPackages.hpack ];
-  preConfigure = ''hpack'';
-  isLibrary = true;
-  isExecutable = true;
   buildInputs = [ pkgs.postgresql pkgs.zlib pkgs.haskellPackages.hpack pkgs.haskellPackages.cabal2nix ];
   src = ./.;
 })
