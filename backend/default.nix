d062e565f5f4cb60e37a4d68d17e7f51f6754654
Jappie Klooster <jappieklooster@hotmail.com>
2018-10-06 15:02:25 +0200

    attempt to use default.nix

--- backend/default.nix
+++ backend/default.nix
@@ -1,10 +1,27 @@
-{ pkgs ? (import ../reflex-platform/nixpkgs {}) }:
-(pkgs.stdenv.mkDerivation {
-  name = "Minimal-example-of";
-  buildTools = [ pkgs.haskellPackages.hpack ];
-  preConfigure = ''hpack'';
+{ mkDerivation, aeson, base, beam-core, beam-migrate, beam-postgres
+, bytestring, hpack, lib, postgresql-simple, servant-server, stdenv
+, text, wai, warp
+}:
+mkDerivation {
+  pname = "awesome-project-name";
+  version = "0.1.0.0";
+  src = ./.;
   isLibrary = true;
   isExecutable = true;
-  buildInputs = [ pkgs.postgresql pkgs.zlib pkgs.haskellPackages.hpack pkgs.haskellPackages.cabal2nix ];
-  src = ./.;
-})
+  libraryHaskellDepends = [
+    aeson base beam-core beam-migrate beam-postgres bytestring
+    postgresql-simple servant-server text wai warp
+  ];
+  libraryToolDepends = [ hpack ];
+  executableHaskellDepends = [
+    aeson base beam-core beam-migrate beam-postgres bytestring
+    postgresql-simple servant-server text wai warp
+  ];
+  testHaskellDepends = [
+    aeson base beam-core beam-migrate beam-postgres bytestring
+    postgresql-simple servant-server text wai warp
+  ];
+  preConfigure = "hpack";
+  homepage = "https://github.com/githubuser/awesome-project-name#readme";
+  license = stdenv.lib.licenses.bsd3;
+}
