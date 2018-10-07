d6cb4534a7794e1366b2f2dfb5a55fcd13302616
Jappie Klooster <jappieklooster@hotmail.com>
2018-10-07 20:46:06 +0200

    Clean up the previous example

--- frontend/src/Lib.hs
+++ frontend/src/Lib.hs
@@ -31,16 +31,7 @@ reflex = do
 
   mainWidget $
     el "div" $ do
-    nx <- numberInput
-    d <- dropdown Times (constDyn ops) def
-    ny <- numberInput
-    let values = zipDynWith (,) nx ny
-        result = zipDynWith (\o (x,y) -> runOp o <$> x <*> y) (_dropdown_value d) values
-        resultText = fmap (pack . show) result
-    text " =dad fadf adfdsf "
-    dynText resultText
-    elClass "div" "int-demo" $ do
-        intButton  <- button "Get Int"
+        intButton  <- button "Get Users"
         serverInts <- fmapMaybe reqSuccess <$> getUsers intButton
         name <- textInput def
         email <- textInput def
@@ -51,36 +42,10 @@ reflex = do
         messages <- fmapMaybe reqSuccess <$> postMessage input sendMsg 
         display =<< holdDyn ([Message (User "none" "none") "ddd"]) messages
 
-        -- postMessage <- fmapMaybe reqSuccess <$> postMessage 
-
 messageInput :: (MonadWidget t m) => m (Dynamic t (Either Text Message))
 messageInput = do
     message <- textInput def
     pure $ join $ mapDyn (Right . (Message $ User "none" "none") . unpack) (_textInput_value message)
-
-numberInput :: (MonadWidget t m) => m (Dynamic t (Maybe Double))
-numberInput = do
-  let errorState = "style" =: "border-color: red"
-      validState = "style" =: "border-color: green"
-  rec n <- textInput $ def & textInputConfig_inputType .~ "number"
-                           & textInputConfig_initialValue .~ "0"
-                           & textInputConfig_attributes .~ attrs
-      let result = readMaybe . unpack <$> _textInput_value n
-          attrs  = fmap (maybe errorState (const validState)) result
-  return result
-
-data Op = Plus | Minus | Times | Divide deriving (Eq, Ord)
-
-ops :: Map Op Text
-ops = Map.fromList [(Plus, "+"), (Minus, "-"), (Times, "*"), (Divide, "/")]
-
-runOp :: Fractional a => Op -> a -> a -> a
-runOp s = case s of
-            Plus -> (+)
-            Minus -> (-)
-            Times -> (*)
-            Divide -> (/)
-
 
 -- | The typesignature of this function is ridiculous, let's just ignore it
 apiClients :: forall t m. (MonadWidget t m) => _
