5f2c9802cab8b8d3ee311c3b984b2b9ce05f65b9
Jappie Klooster <jappieklooster@hotmail.com>
2019-11-28 23:00:54 +0100

    Add checks for brittany and hlint

---
+++ travis-shell.nix
@@ -0,0 +1,16 @@
+{ pkgs ? import (builtins.fetchGit {
+    # newer pin allows britanny
+    name = "nixos-pin-19.10.2019";
+    url = https://github.com/nixos/nixpkgs/;
+    rev = "8253c106c63f575669da61c41914891e55f68881";
+}) { }}:
+
+# Tools we need for testing if the source code is sane.
+# Not neccisarly needed for the project
+pkgs.mkShell{
+    buildInputs = [
+        pkgs.hlint
+        pkgs.haskellPackages.brittany
+        pkgs.findutils
+    ];
+}
